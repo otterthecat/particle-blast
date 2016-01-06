19cc8d02543545104998e54f96b12e2e7057a8c3
Otter <otc@otterthecat.com>
2016-01-06 05:25:08 -0600

    fix broken test. add test for throwing error

--- test/specs/blastSpec.js
+++ test/specs/blastSpec.js
@@ -58,18 +58,24 @@ describe('Particle-Blast', function () {
       describe('The middleware function', function () {
 
         it('should call the #beam() function', function () {
-          blast.fire(action, parse, callback);middleware(req, res, next);
+          middleware(req, res, next);
+          blast.beam.should.have.been.calledOnce;
           blast.beam.should.have.been.calledWith(action, req.originalUrl, callback);
         });
 
         it('should call the passed parse() argument', function () {
           middleware(req, res, next);
+          parse.should.have.been.calledOnce;
           parse.should.have.been.calledWith(req);
         });
 
         it('should call the passed next() argument', function () {
           middleware(req, res, next);
-          next.should.have.been.called;
+          next.should.have.been.calledOnce;
+        });
+
+        it('should throw error when passed action is not a string', function () {
+          blast.fire.should.throw(Error);
         });
       });
     });
